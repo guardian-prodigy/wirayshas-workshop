0dd8f776c5e968dbdccd8707e734c7c6d5b0aea8
guardian-prodigy <devilmusicmutant@gmail.com>
2021-09-17 13:50:22 -0300

    added lazy loading

--- dist/letters.b59e07d8.js
+++ dist/letters.b59e07d8.js
@@ -407,7 +407,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53387" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63105" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
