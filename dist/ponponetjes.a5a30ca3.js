c27c57aecfd4077dafdfc425536c68e80b069414
guardian-prodigy <devilmusicmutant@gmail.com>
2021-09-23 16:10:20 -0300

    added overflow-x: hidden to body

--- dist/ponponetjes.a5a30ca3.js
+++ dist/ponponetjes.a5a30ca3.js
@@ -317,7 +317,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58425" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58733" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
